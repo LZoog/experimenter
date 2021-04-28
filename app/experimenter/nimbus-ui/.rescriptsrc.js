cba6d4fbbfc671c43ec917e33910e7f34a6d226b
Lauren Zugai <lauren@zugai.com>
2021-04-28 13:39:37 -0500

    Moar progress, rescripts separate css bundle works

--- app/experimenter/nimbus-ui/.rescriptsrc.js
+++ app/experimenter/nimbus-ui/.rescriptsrc.js
@@ -2,31 +2,45 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
 // Rescripts allows us modify the default CRA
 // Webpack config without needing to eject.
 // Check out the README for how to use this file.
 
-module.exports = [(config) => {
-  // Disable code splitting entirely
-  config.optimization.runtimeChunk = false;
-  config.optimization.splitChunks = {
-    cacheGroups: {
-      default: false,
-    },
-  };
+module.exports = [
+  (config) => {
+    // Our 404 page has a lot of unique CSS and calls for a separate bundle
+    // FixStyleOnlyEntriesPlugin removes the empty generated JS file
+    config.entry = {
+      main: [...config.entry],
+      404: ["./src/styles/404.scss"],
+    };
 
-  // Always use a consistent output name, without
-  // contenthash, so we can reliably locate it
-  // from within a Django template
-  config.output.filename = "static/js/[name].js";
-  // We need to do this for CSS as well
-  config.plugins[
-    config.plugins.findIndex((p) => p.constructor.name === 'MiniCssExtractPlugin')
-  ] = new MiniCssExtractPlugin({
-    filename: 'static/css/[name].css',
-  });
+    // Disable code splitting entirely
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
 
-  return config;
-}];
+    // Always use a consistent output name, without
+    // contenthash, so we can reliably locate it
+    // from within a Django template
+    config.output.filename = "static/js/[name].js";
+    // We need to do this for CSS as well
+    config.plugins[
+      config.plugins.findIndex(
+        (p) => p.constructor.name === "MiniCssExtractPlugin",
+      )
+    ] = new MiniCssExtractPlugin({
+      filename: "static/css/[name].css",
+    });
+
+    config.plugins = [...config.plugins, new FixStyleOnlyEntriesPlugin()];
+
+    return config;
+  },
+];
